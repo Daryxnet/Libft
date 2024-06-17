a658486a270cf5310e2656d9cdede7a6fcd6b325
Daryxnet <154624810+Daryxnet@users.noreply.github.com>
2024-06-17 15:27:03 +0200

    Add files via upload

--- LibftPregit/main.c
+++ LibftPregit/main.c
@@ -1,16 +1,14 @@
 #include "libft.h"
 #include <stdio.h>
 
-//int	ft_isdigit(int c);
-
 int	main()
 {
 	char	let;
 
-	let = '5';
+	let = 'G';
 	if (ft_isalpha(let))
 		printf("es una letra\n");
-	if (ft_isdigit(let))
+	else if (ft_isdigit(let))
 		printf("es un digito");
 	else
 	      printf("no es ni letra ni digito");
