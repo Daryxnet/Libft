6009400edaae8d17205e1cffb7ec54c7dfa5dbfd
Daryxnet <154624810+Daryxnet@users.noreply.github.com>
2024-06-20 16:09:34 +0200

    Add files via upload

---
+++ ft_memset.c
@@ -0,0 +1,17 @@
+
+#include "libft.h"
+
+void	*ft_memset(void *ptr, int x, size_t n)
+{
+	unsigned char	*s;
+	size_t			i;
+
+	i = 0;
+	s = ptr;
+	while (i < n)
+	{
+		s[i] = (unsigned char)x; 
+		i++;
+	}
+	return (ptr);
+}
